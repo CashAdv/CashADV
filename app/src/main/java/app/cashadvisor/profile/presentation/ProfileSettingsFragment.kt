4fc697b01821e60a682277766520e171e61a562f
ledrabbit <lvzaytseva1@gmail.com>
2024-03-19 19:05:00 +0300

    added profile pic upload

--- app/src/main/java/app/cashadvisor/profile/presentation/ProfileSettingsFragment.kt
+++ app/src/main/java/app/cashadvisor/profile/presentation/ProfileSettingsFragment.kt
@@ -1,22 +1,60 @@
 package app.cashadvisor.profile.presentation
 
+import android.net.Uri
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import app.cashadvisor.common.ui.BaseFragment
 import app.cashadvisor.databinding.FragmentProfileSettingsBinding
+import app.cashadvisor.uikit.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileSettingsFragment() :
+class ProfileSettingsFragment :
     BaseFragment<FragmentProfileSettingsBinding, ProfileSettingsViewModel>(
         FragmentProfileSettingsBinding::inflate
     ) {
+
     override val viewModel: ProfileSettingsViewModel by viewModels()
 
+    private var profilePicUri: Uri? = null
+
     override fun onConfigureViews() {
-        TODO("Not yet implemented")
+        setChangePictureViewClickListener()
     }
 
     override fun onSubscribe() {
-        TODO("Not yet implemented")
+
+    }
+
+    private fun setChangePictureViewClickListener() {
+        val pickMedia = registerPickMediaRequest()
+        binding.llChangeProfilePic.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+    }
+
+    private fun registerPickMediaRequest() =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                profilePicUri = uri
+                setImageToIV(uri)
+            }
+        }
+
+    private fun setImageToIV(uri: Uri) {
+        Glide.with(this)
+            .load(uri)
+            .placeholder(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.placeholder_profile_picture
+                )
+            )
+            .apply(RequestOptions.circleCropTransform())
+            .into(binding.ivProfilePic)
     }
 }
