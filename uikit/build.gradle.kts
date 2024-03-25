5704d6ef68de96386bc58cab0117413d0b1d51ac
ledrabbit <lvzaytseva1@gmail.com>
2024-03-25 15:41:12 +0300

    added glide dependency for profile header

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -38,6 +38,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
