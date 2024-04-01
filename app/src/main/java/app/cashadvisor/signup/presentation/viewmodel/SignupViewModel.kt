b891122130f89a5994ae5ad2c56fe33ead94c905
Aleksey Putilov <putilov0407@gmail.com>
2024-04-01 22:01:27 +0500

    Add registration and code error handling to your email

--- app/src/main/java/app/cashadvisor/signup/presentation/viewmodel/SignupViewModel.kt
+++ app/src/main/java/app/cashadvisor/signup/presentation/viewmodel/SignupViewModel.kt
@@ -1,7 +1,6 @@
 package app.cashadvisor.signup.presentation.viewmodel
 
 import androidx.lifecycle.viewModelScope
-import app.cashadvisor.R
 import app.cashadvisor.authorization.domain.api.InputValidationInteractor
 import app.cashadvisor.authorization.domain.api.RegisterInteractor
 import app.cashadvisor.authorization.domain.models.ConfirmCode
@@ -30,7 +29,6 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
