66209990446fac83846774b7f6c20eb783b7f630
MajoritySky2496 <gzag2496@gmail.com>
2024-05-22 23:34:41 +0500

    100-Main_Page_Repository 1) Made models 2) Made ApiService

---
+++ app/src/main/java/app/cashadvisor/profile/data/dto/response/ProfileAnalyticsResponse.kt
@@ -0,0 +1,57 @@
+package app.cashadvisor.profile.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProfileAnalyticsResponse (
+    @SerialName("status_code") val statusCode: Int,
+    val message: String,
+    @SerialName("response_currency") val responseCurrency: String,
+    @SerialName("profile") val userAnalyticsDto: UserAnalyticsDto
+)
+
+@Serializable
+data class UserAnalyticsDto(
+    @SerialName("income") val incomeDto: List<IncomeDto>,
+    @SerialName("expense") val expenseDto: List<ExpenseDto>,
+    @SerialName("wealth_fund") val wealthFundDto: List<WealthFundDto>
+)
+
+@Serializable
+data class IncomeDto(
+    val amount: Int,
+    @SerialName("category_id") val categoryId: String,
+    val date: String,
+    val id: String,
+    val planned: Boolean,
+    @SerialName("user_id") val userId: String,
+    @SerialName("bank_account") val bankAccount: String,
+    val sender: String,
+    val currency: String
+)
+
+@Serializable
+data class ExpenseDto(
+    val amount: Int,
+    @SerialName("category_id") val categoryId: String,
+    val date: String,
+    val id: String,
+    val planned: Boolean,
+    @SerialName("user_id") val userId: String,
+    @SerialName("bank_account") val bankAccount: String,
+    @SerialName("sent_to") val sentTo: String,
+    val currency: String
+)
+
+@Serializable
+data class WealthFundDto(
+    val amount: Int,
+    val date: String,
+    val id: String,
+    val planned: Boolean,
+    @SerialName("user_id") val userId: String,
+    @SerialName("bank_account") val bankAccount: String,
+    val currency: String,
+    @SerialName("category_id") val categoryId: String
+)
