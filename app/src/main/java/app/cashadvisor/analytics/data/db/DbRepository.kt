705b38a8eb342a9641ef4596f80455b662c818e6
ValentinPside <valentin.pside@mail.ru>
2024-06-14 21:43:56 +0300

    Реализована сводная таблица, составлены и проверены тестовые запросы с различными параметрами, получены тестовые списки в App

--- app/src/main/java/app/cashadvisor/analytics/data/db/DbRepository.kt
+++ app/src/main/java/app/cashadvisor/analytics/data/db/DbRepository.kt
@@ -1,8 +1,10 @@
 package app.cashadvisor.analytics.data.db
 
 import app.cashadvisor.analytics.data.db.entities.CategoryEntity
+import app.cashadvisor.analytics.data.db.entities.CategoryWithUserAnalyticsEntity
 import app.cashadvisor.analytics.data.db.entities.UserAnalyticsEntity
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 class DbRepository(private val dao: Dao) {
@@ -26,4 +28,35 @@ class DbRepository(private val dao: Dao) {
             dao.upsertCategoryEntity(categoryEntity)
         }
     }
+
+    suspend fun upsertCategoryWithUserAnalytics(categoryWithUserAnalyticsEntity: List<CategoryWithUserAnalyticsEntity>){
+        withContext(Dispatchers.IO){
+            dao.upsertCategoryWithAnalyticsEntity(categoryWithUserAnalyticsEntity)
+        }
+    }
+
+    suspend fun getCategoryWithUserAnalyticsEntityList(): List<CategoryWithUserAnalyticsEntity>{
+        return dao.getCategoryWithUserAnalyticsEntityList()
+    }
+
+    suspend fun getByCategoryName(categoryName: String): List<CategoryWithUserAnalyticsEntity>{
+        return dao.getByCategoryName(categoryName)
+    }
+
+    suspend fun getByDate(date: String): List<CategoryWithUserAnalyticsEntity>{
+        return dao.getByDate(date)
+    }
+
+    suspend fun getByTwoDate(dateStart: String, dateEnd: String): List<CategoryWithUserAnalyticsEntity>{
+        return dao.getByTwoDate(dateStart, dateEnd)
+    }
+
+    suspend fun getByTwoAmount(minAmount: Int, maxAmount: Int): List<CategoryWithUserAnalyticsEntity> {
+        return dao.getByTwoAmount(minAmount, maxAmount)
+    }
+
+    suspend fun getMoreThenMinAmount(minAmount: Int): List<CategoryWithUserAnalyticsEntity>{
+        return dao.getMoreThenMinAmount(minAmount)
+    }
+
 }
